b4fd2497b376e4ec599d07b4b0055a8dae13febb
Daria Zhuravleva <darzhur@MacBook-Air-Daria.local>
2026-03-02 19:47:36 +0300

    Правки по запуску

--- mcp_server.py
+++ mcp_server.py
@@ -11,5 +11,9 @@ async def get_memes():
     # твой код для мемов
     return []
 
+# тест
 # uvicorn app:app --reload --host 127.0.0.1 --port 8000
 # http://127.0.0.1:8000/memes
+
+# прод
+# uvicorn app:app --host 0.0.0.0 --port $PORT
